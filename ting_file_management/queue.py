64f9b28eaf075e271ec29d9789a45418ce8c7924
Marcelo Ataíde <msa.trybe@gmail.com>
2023-05-09 08:29:13 -0300

    Requisito 1 - Implementa uma fila para armazenar os arquivos que serão lidos.

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -3,17 +3,23 @@ from ting_file_management.abstract_queue import AbstractQueue
 
 class Queue(AbstractQueue):
     def __init__(self):
-        """Iniciando Projeto TING"""
-        """Inicialize sua estrutura aqui"""
+        self._data = list()
+        self._length = 0
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return self._length
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self._data.append(value)
+        self._length += 1
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        if len(self._data) == 0:
+            return None
+        self._length -= 1
+        return self._data.pop(0)
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if index < 0 or index >= self._length:
+            raise IndexError("Índice Inválido ou Inexistente")
+        return self._data[index]
