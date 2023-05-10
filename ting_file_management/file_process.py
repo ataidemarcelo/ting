77ef0360a24ae7662e7fcd9ac3e87a0aa5f2fb6c
Marcelo Ataíde <msa.trybe@gmail.com>
2023-05-10 12:52:08 -0300

    Requisito 3 - Implementa a função `process` no módulo file_process.

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,23 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for i in range(len(instance)):
+        if instance.search(i)['nome_do_arquivo'] == path_file:
+            print("Arquivo já processado", file=sys.stderr)
+            return
+    
+    lines = txt_importer(path_file)
+
+    data = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(lines),
+        "linhas_do_arquivo": lines
+    }
+
+    instance.enqueue(data)
+    print(data)
 
 
 def remove(instance):
