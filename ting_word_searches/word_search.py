0ea77f90e7e70fdaeb699a8218778bbd0c8304e8
Marcelo Ataíde <msa.trybe@gmail.com>
2023-05-12 10:00:00 -0300

    Requisitos 7, 8 - Implementa as funções `exists_word` e `search_by_word`

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,44 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    word = word.lower()
+    result = []
+
+    for i in range(len(instance)):
+        file_info = instance.search(i)
+        lines = file_info['linhas_do_arquivo']
+        occurrences = []
+
+        for index, line in enumerate(lines):
+            if word in line.lower():
+                occurrences.append({"linha": index + 1})
+
+        if occurrences:
+            result.append({
+                "palavra": word,
+                "arquivo": file_info['nome_do_arquivo'],
+                "ocorrencias": occurrences
+            })
+
+    return result
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    word = word.lower()
+    result = []
+
+    for i in range(len(instance)):
+        file_info = instance.search(i)
+        lines = file_info['linhas_do_arquivo']
+        occurrences = []
+
+        for index, line in enumerate(lines):
+            if word in line.lower():
+                occurrences.append({"linha": index + 1, "conteudo": line})
+
+        if occurrences:
+            result.append({
+                "palavra": word,
+                "arquivo": file_info['nome_do_arquivo'],
+                "ocorrencias": occurrences
+            })
+
+    return result
